96fbf826b22907ae9f924618df2692ac0e149e92
andreas <podovinov2003andrey@gmail.com>
2023-04-24 10:48:23 +0600

    added base structure

---
+++ server/src/api/base_router.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+# from src.api import ...
+
+
+base_router = APIRouter()
---
+++ server/src/core/settings.py
@@ -0,0 +1,21 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    host: str = 'localhost'
+    port: int = 9999
+    docs_url: str = '/docs'
+    redoc_url: str = '/redoc'
+    
+    db_login: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_database: str
+
+    class Config:
+        env_file = '../.env'
+        env_file_encoding = 'utf-8'
+
+
+settings = Settings()
